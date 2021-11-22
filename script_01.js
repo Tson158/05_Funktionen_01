80ffbf72b05bcb8213e4564bfba51a7d35c05581
Kakarott <Sonntag_thomas_86@web.de>
2021-11-22 10:39:20 +0100

    funktionen ++

--- script_01.js
+++ script_01.js
@@ -1,7 +1,6 @@
 /***** Funktionen 01 *****/
 // 1. Kappselung von Codeblöcken
 
-<<<<<<< HEAD
 //  Funktionsaufruf (call/er)
 
 test();
@@ -14,18 +13,9 @@ function test()
     console.log("Hallo Thomas!");
 
 }
-=======
-// 1. Kapselung von Codeblöcken
 
-// Funktionsaufruf (call)
-// test();
 
-// Funktionsrumpf (body) | callee
-function test()
-{
-    console.log("Hallo Anton!");
+function ausgabeNamen(){
+    console.log("Hallo Thomas!");
+
 }
-
-
-
->>>>>>> 956ad2583c358b89ac10ecd6116d9907febcea0b
