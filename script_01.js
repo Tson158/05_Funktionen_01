956ad2583c358b89ac10ecd6116d9907febcea0b
Peter <schmidt.multimedia@gmail.com>
2021-11-22 10:22:33 +0100

    01 / Kapselung von Codeblöcken

--- script_01.js
+++ script_01.js
@@ -1,2 +1,15 @@
 /***** Funktionen 01 *****/
 
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf (call)
+// test();
+
+// Funktionsrumpf (body) | callee
+function test()
+{
+    console.log("Hallo Anton!");
+}
+
+
+
