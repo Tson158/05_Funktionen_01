ab164b6442ffe2c32509922c081a2f5cbddfc1dd
Kakarott <Sonntag_thomas_86@web.de>
2021-11-22 12:02:44 +0100

    const Variablen++

--- script_01.js
+++ script_01.js
@@ -6,23 +6,23 @@
 // test();
 
 // Funktionsrumpf (body) | callee
-function test()
-{
-    console.log("Hallo Thomas!");
-}
+// function test()
+// {
+//     console.log("Hallo Thomas!");
+// }
 
 
-// Funktionen 2a
-// Parametisierung + Datenübergabe von innen
+// // Funktionen 2a
+// // Parametisierung + Datenübergabe von innen
 
-ausgabeNamen();
+// ausgabeNamen();
 
-function ausgabeNamen()
- {
-    let firstName = "Thomas";
-    console.log("Hallo " + firstName + "!");
-    console.log(firstName);
-}
+// function ausgabeNamen()
+//  {
+//     let firstName = "Thomas";
+//     console.log("Hallo " + firstName + "!");
+//     console.log(firstName);
+// }
 
 // console.log(firstName);
 
@@ -32,9 +32,19 @@ function ausgabeNamen()
 
 function ausgabeNamenParam(firstName) 
 {
-    
+    const cond1 = (firstName == "")
+    const cond2 = (firstName == undefined)
+
+
+    if(cond1 || cond2)
+    {
+     firstName = "Nobody";
+    }
+
     console.log("Hallo " + firstName + "!");
 
 }
 ausgabeNamenParam("Mia")
 ausgabeNamenParam("Melanie")
+ausgabeNamenParam()
+ausgabeNamenParam(prompt("Bitte gib dein Vornamen ein :"));
