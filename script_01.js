5692097e2d4b4bee2f49c9e6f522f7bf9550cdb6
Kakarott <Sonntag_thomas_86@web.de>
2021-11-22 10:23:07 +0100

    Kappselung von Codeblöcken

--- script_01.js
+++ script_01.js
@@ -1,2 +1,15 @@
 /***** Funktionen 01 *****/
+// 1. Kappselung von Codeblöcken
 
+//  Funktionsaufruf (call/er)
+
+test();
+
+// Funktionsrumpf (body) | callee
+
+function test()
+{
+
+    console.log("Hallo Thomas!");
+
+}
