a7d052813707b07741a9b01017fcf40be542c712
Kakarott <Sonntag_thomas_86@web.de>
2021-11-22 11:17:30 +0100

    2b++

--- script_01.js
+++ script_01.js
@@ -1,21 +1,40 @@
-/***** Funktionen 01 *****/
-// 1. Kappselung von Codeblöcken
+//  Funktionen 01 *****/
 
-//  Funktionsaufruf (call/er)
+// 1. Kapselung von Codeblöcken
 
-test();
+// Funktionsaufruf (call)
+// test();
 
 // Funktionsrumpf (body) | callee
-
 function test()
 {
-
     console.log("Hallo Thomas!");
-
 }
 
 
-function ausgabeNamen(){
-    console.log("Hallo Thomas!");
+// Funktionen 2a
+// Parametisierung + Datenübergabe von innen
+
+ausgabeNamen();
+
+function ausgabeNamen()
+ {
+    let firstName = "Thomas";
+    console.log("Hallo " + firstName + "!");
+    console.log(firstName);
+}
+
+// console.log(firstName);
+
+
+/*********** Funktion 2b ********/
+// 2b. Parametisierung + Datenünergabe von Aussen
+
+function ausgabeNamenParam(firstName) 
+{
+    
+    console.log("Hallo " + firstName + "!");
 
 }
+ausgabeNamenParam("Mia")
+ausgabeNamenParam("Melanie")
